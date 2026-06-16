39a32c864007509e3d8d675cfacca38ebdf95c4f
diego <dcervantesapa@unsa.edu.pe>
2026-06-16 10:28:39 -0500

    Crear vista protegida en views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def saludo(request):
+
+    return Response({
+        "mensaje": "Autenticación JWT correcta"
+    })
