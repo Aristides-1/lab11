17e5ddd111f89c96e1af0160d07a27a0c4d8cbc7
diego <dcervantesapa@unsa.edu.pe>
2026-06-18 23:24:09 -0500

    Agregar ruta de productos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
+
 import { Login } from './login/login';
+import { Productos } from './productos/productos';
 
 export const routes: Routes = [
+
   {
     path: '',
     component: Login
+  },
+
+  {
+    path: 'productos',
+    component: Productos
   }
+
 ];
