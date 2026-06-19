c26f1fc340036f73c0c803288f37b9c7669814d0
diego <dcervantesapa@unsa.edu.pe>
2026-06-18 23:23:17 -0500

    plantilla de productos html

--- frontend/src/app/productos/productos.ts
+++ frontend/src/app/productos/productos.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductoService } from '../services/producto';
 
 @Component({
   selector: 'app-productos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './productos.html',
   styleUrl: './productos.css',
 })
-export class Productos {}
+export class Productos implements OnInit {
+
+  productos: any[] = [];
+
+  constructor(private productoService: ProductoService) {}
+
+  ngOnInit(): void {
+
+    this.productoService.obtenerProductos().subscribe(
+      (data: any) => {
+
+        this.productos = data;
+
+        console.log(data);
+
+      }
+    );
+
+  }
+
+}
