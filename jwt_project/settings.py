f40fbcb82fc84f4526054b79d51d7d6ffcfac43a
diego <dcervantesapa@unsa.edu.pe>
2026-06-18 23:14:03 -0500

    requerimientos de cors finales

--- jwt_project/settings.py
+++ jwt_project/settings.py
@@ -49,6 +49,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
