accac4f9830f1dac284f78ba168ce89bdd47ad23
diego <dcervantesapa@unsa.edu.pe>
2026-06-18 23:05:23 -0500

    LoginComponent  Abre:
    construccion de logica del componente login

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AuthService } from '../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  username = '';
+  password = '';
+
+  constructor(private auth: AuthService) {}
+
+  login() {
+
+    const data = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.auth.login(data).subscribe((res: any) => {
+
+      localStorage.setItem('token', res.access);
+
+      console.log('Login correcto');
+
+      console.log(res);
+
+    });
+
+  }
+
+}
