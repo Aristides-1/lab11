4f7a88c3778c09385872a57c205ade50b69f37ab
diego <dcervantesapa@unsa.edu.pe>
2026-06-26 00:14:55 -0500

    app.config.ts evitar zonejs

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core'; // <-- Cambiado aquí
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; 
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZonelessChangeDetection(), // <--- Y cambiado aquí
     provideRouter(routes),
-    provideHttpClient() 
+    provideHttpClient()
   ]
 };
