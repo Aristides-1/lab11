f97260a3cc2eca3df1c5460893063de9c3d7627d
diego <dcervantesapa@unsa.edu.pe>
2026-06-26 00:28:30 -0500

    mapeo de direcciones URL a componentes
    router.navigate para renderizar los nuevos componentes reactivos sin problema *con zoneless*

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { LoginComponent } from './login/login';
 import { ProductosComponent } from './productos/productos';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'productos', component: ProductosComponent }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent }, // <-- Ojo: Debe ser LoginComponent
+  { path: 'productos', component: ProductosComponent },
+  { path: '**', redirectTo: 'login' }
 ];
