d92b399af531b47ecdf76d16964efaab559b114e
diego <dcervantesapa@unsa.edu.pe>
2026-06-25 23:10:38 -0500

    limpieza bloque de citas login.ts

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -6,32 +6,32 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule], 
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class LoginComponent {
-  username = ''; [cite: 251]
-  password = ''; [cite: 252]
+  username = '';
+  password = '';
 
-  constructor(private auth: AuthService, private router: Router) {} [cite: 253]
+  constructor(private auth: AuthService, private router: Router) {}
 
-  login() { [cite: 254]
-    const data = { [cite: 255]
-      username: this.username, [cite: 258]
-      password: this.password [cite: 259]
-    }; [cite: 256]
+  login() {
+    const data = {
+      username: this.username,
+      password: this.password
+    };
 
     this.auth.login(data).subscribe({
       next: (res: any) => {
-        localStorage.setItem('token', res.access); [cite: 261]
-        console.log('Login correcto'); [cite: 262]
-        this.router.navigate(['/productos']); // Redirige tras iniciar sesión correctamente
+        localStorage.setItem('token', res.access);
+        console.log('Login correcto');
+        this.router.navigate(['/productos']);
       },
       error: (err) => {
         console.error('Error en el login:', err);
         alert('Credenciales inválidas o error de conexión.');
       }
-    }); [cite: 260]
+    });
   }
 }
