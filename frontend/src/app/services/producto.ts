5cd9868bd3b2906f63eb0e2d8a85f97625f72756
diego <dcervantesapa@unsa.edu.pe>
2026-06-25 23:03:00 -0500

    producto.ts modificacion
    Corrige la referencia de las cabeceras para que coincida exactamente con lo solicitado por el Backend

--- frontend/src/app/services/producto.ts
+++ frontend/src/app/services/producto.ts
@@ -1,23 +1,21 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
+  private apiUrl = 'http://127.0.0.1:8000/api/productos/'; 
 
-  apiUrl = 'http://127.0.0.1:8000/api/productos/';
-
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   obtenerProductos() {
-
+    const token = localStorage.getItem('token');
     const headers = new HttpHeaders({
-      Authorization: 'Bearer ' + localStorage.getItem('token')
+      'Authorization': `Bearer ${token}` 
     });
 
-    return this.http.get(this.apiUrl, { headers });
-
+    // Pasamos el objeto de configuración correctamente con la variable corregida
+    return this.http.get(this.apiUrl, { headers: headers }); 
   }
-
 }
